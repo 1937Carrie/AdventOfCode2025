f6f1ccf11e5bb6944a1bf5849c1e956eaafa2615
Stanislav Dumchykov <mechanikea@gmail.com>
2025-12-05 16:19:43 +0200

    Added second part of Day05

--- Day05/Day05.kt
+++ Day05/Day05.kt
@@ -1,29 +1,46 @@
 import java.io.File
 
 fun main() {
-    val input = File("Day05.txt").readLines()
+    val input = File("datasets/Day05").readLines()
 
     println(resolvePart1(input))
     println(resolvePart2(input))
 }
 
-fun resolvePart2(input: List<String>): Int {
-    val (tmpFreshIngredientIDs, tmpAvailableIngredientIDs) =
+fun resolvePart2(input: List<String>): Long {
+    val (tmpFreshIngredientIDs, _) =
         input.filter(String::isNotBlank).partition { it.contains("-") }
     val freshIngredientIDs = tmpFreshIngredientIDs.map {
         val range = it.split("-")
         range[0].toLong().rangeTo(range[1].toLong())
     }
-    val availableIngredientIDs = tmpAvailableIngredientIDs.map { it.toLong() }
-    TODO()
-    return availableIngredientIDs
-        .asSequence()
-        .flatMap { availableIngredientID ->
-            freshIngredientIDs.filter { it.contains(availableIngredientID) }
+
+    val sortedFresh = freshIngredientIDs.sortedBy { it.first }
+
+    fun two2one(a: Pair<Long, Long>?, b: Pair<Long, Long>): Pair<Long, Long>? {
+        return when {
+            a == null -> b
+            b.first > a.second -> null
+            else -> Pair(minOf(a.first, b.first), maxOf(a.second, b.second))
         }
-        .flatMap { it.asSequence() }
-        .distinct()
-        .sum()
+    }
+
+    val merged = sortedFresh.fold(mutableListOf<Pair<Long, Long>>()) { acc, e ->
+        if (acc.isEmpty()) {
+            acc.add(e.first to e.last)
+        } else {
+            val last = acc.last()
+            val mixed = two2one(last, e.first to e.last)
+            if (mixed != null) {
+                acc[acc.size - 1] = mixed
+            } else {
+                acc.add(e.first to e.last)
+            }
+        }
+        acc
+    }
+
+    return merged.sumOf { (start, end) -> end - start + 1 }
 }
 
 fun resolvePart1(input: List<String>): Int {
